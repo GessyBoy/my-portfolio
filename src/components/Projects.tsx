573ec6e4ea5950d6b1363f4ded05c35eaba693db
GessyBoy <ydeletrez@gmail.com>
2023-11-28 23:38:59 +0100

    edit Projects

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -7,12 +7,14 @@ const Projects = (): JSX.Element => {
       <div className="projects-grid">
         {projectData && projectData.map((project) => (
           <div className="project-card" key={project.id}>
-            <a href={project.gitHubLink} target="_blank">
+            <a href={project.Link} target="_blank">
               <div className="project-header">
                 <i className="fa-regular fa-folder-open folder-icon"></i>
-                <div className="small-icons">
-                  <i className="fa-brands fa-github"></i>
-                </div>
+               {project.id === 3 && (
+                  <div className="small-icons">
+                    <i className="fa-brands fa-github"></i>
+                  </div>
+                )}
               </div>
               <h3>{project.title}</h3>
               <p>{project.description}</p>
--- src/components/data.js
+++ src/components/data.js
@@ -1,22 +1,22 @@
 export const projectData = [
+  {
+    id: 1,
+    title: 'Jimo',
+    description:
+      'JIMO: Your comprehensive real estate partner, currently under development to provide innovative and efficient management of your properties.',
+    Link: 'https://www.j-imo.com/',
+  },
+  {
+    id: 2,
+    title: 'Premier Cap',
+    description:'Welcome to Premier Cap: Currently transforming, this online academic guidance platform lets you select a professional coach and collaboratively build a study plan tailored to your personality, motivations, desires, and ambitions!',
+    Link: 'https://www.premiercap.fr/',
+  },
     {
-      id: 1,
+      id: 3,
       title: 'DC-Comiques',
       description:
         'Developer friend, a game where you are the hero.Would you manage to go to the end of your mission ?',
-      gitHubLink: 'https://github.com/WildCodeSchool/2022-11-JS-RemoteFR-Geekrew-P2-DC-Comiques',
+      Link: 'https://github.com/WildCodeSchool/2022-11-JS-RemoteFR-Geekrew-P2-DC-Comiques',
     },
-    {
-      id: 2,
-      title: 'Externatic',
-      description:
-        'On behalf of an "Externatic" client, an IT recruitment firm.',
-      gitHubLink: 'https://github.com/WildCodeSchool/2022-11-JS-RemoteFR-Geekrew-P3-Externatic',
-    },
-    {
-      id: 3,
-      title: 'Meteo Checker',
-      description:'Meteo_checker is a user-friendly and convenient weather app that provides accurate weather information for any location in the world.',
-      gitHubLink: 'https://github.com/GessyBoy/Meteo_checker',
-    }
   ];
