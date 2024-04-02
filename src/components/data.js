f75dbbbaebfc1136d92354f7ba8a865f4f4fa817
GessyBoy <ydeletrez@gmail.com>
2024-04-02 13:00:58 +0200

    edit project and iteration data

--- src/components/data.js
+++ src/components/data.js
@@ -1,22 +1,22 @@
 export const projectData = [
   {
     id: 1,
-    title: 'Jimo',
+    title: "De Laure et d'épices",
     description:
-      'JIMO: Your comprehensive real estate partner, currently under development to provide innovative and efficient management of your properties.',
-    Link: 'https://www.j-imo.com/',
+      "Embark on a journey through De Laure et d'Épices, an e-commerce platform dedicated to offering high-quality organic teas, crafted with a commitment to responsible agriculture.",
+    Link: "https://www.delaureetdepices.fr/",
   },
   {
     id: 2,
-    title: 'Premier Cap',
-    description:'Welcome to Premier Cap: Currently transforming, this online academic guidance platform lets you select a professional coach and collaboratively build a study plan tailored to your personality, motivations, desires, and ambitions!',
-    Link: 'https://www.premiercap.fr/',
+    title: "Premier Cap",
+    description:"Welcome to Premier Cap: Currently transforming, this online academic guidance platform lets you select a professional coach and collaboratively build a study plan tailored to your personality, motivations, desires, and ambitions!",
+    Link: "https://www.premiercap.fr/",
   },
     {
       id: 3,
-      title: 'DC-Comiques',
+      title: "DC-Comiques",
       description:
-        'Developer friend, a game where you are the hero.Would you manage to go to the end of your mission ?',
-      Link: 'https://github.com/WildCodeSchool/2022-11-JS-RemoteFR-Geekrew-P2-DC-Comiques',
+        "Developer friend, a game where you are the hero.Would you manage to go to the end of your mission ?",
+      Link: "https://github.com/WildCodeSchool/2022-11-JS-RemoteFR-Geekrew-P2-DC-Comiques",
     },
   ];
