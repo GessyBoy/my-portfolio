c495b923fcb0456eb98648d26e52f00f5202367b
GessyBoy <ydeletrez@gmail.com>
2024-11-03 22:48:13 +0100

    modif space text

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -6,7 +6,8 @@ const About = (): JSX.Element => {
       <h2>À propos</h2>
       <div className="flex-about">
         <div className="about-text">
-        <p> Développeur passionné, je conçois des solutions adaptées aux besoins de mes clients en utilisant des technologies comme JavaScript avec React, Angular, et Node.js avec Expressjs. J'assure également la maintenance de sites WordPress en PHP. </p> <p> Mon objectif est de mettre ces outils au service des besoins de mes clients, en optimisant les performances et l'expérience utilisateur, tout en assurant un code de qualité. </p>
+        <p> Développeur passionné, je conçois des solutions adaptées aux besoins de mes clients en utilisant des technologies comme JavaScript avec React, Angular, et Node.js avec Expressjs. J'assure également la maintenance de sites WordPress en PHP. </p> 
+        <p> Mon objectif est de mettre ces outils au service des besoins de mes clients, en optimisant les performances et l'expérience utilisateur, tout en assurant un code de qualité. </p>
         </div>
         <div className="about-img">
         <Image src='/on_ouvre_ou_pas.jpg' alt='about-me' className="profile-img" width={300} height={500} />
