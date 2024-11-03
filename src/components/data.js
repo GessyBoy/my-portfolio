53263b76f8016cffcc8b5da068a882b8277b67ca
GessyBoy <ydeletrez@gmail.com>
2024-11-03 22:29:20 +0100

    update portfolio data

--- src/components/data.js
+++ src/components/data.js
@@ -1,26 +1,33 @@
 export const projectData = [
   {
     id: 1,
+    title: "Ceformed",
+    description:
+      "Ceformed est un centre de formation spécialisé dans le Développement Professionnel Continu (DPC) pour les professionnels de santé, visant à actualiser leurs connaissances et améliorer la prise en charge des patients. Créé en 2017, il propose des formations adaptées à tous les statuts d’exercice en santé.",
+    Link: "https://www.ceformed.fr/",
+  },
+  {
+    id: 2,
+    title: "Coloorfool",
+    description:"Coolorfool est une marque de chaussettes et mi-bas fabriqués en Italie à partir de fil d’Écosse et de laine mérinos. Les produits sont confectionnés dans un atelier familial, garantissant ainsi un savoir-faire artisanal de qualité. La marque propose ses articles via un site e-commerce.",
+    Link: "https://coolorfool.eu/index.php?",
+  },
+  {
+    id: 3,
     title: "J-imo",
     description:
       "Gestion locative. JIMO automatise vos tâches chronophages tout en offrant un soutien humain pour une gestion simplifiée et efficace de vos biens.",
     Link: "https://www.j-imo.com/",
   },
-  {
-    id: 2,
-    title: "Premier Cap",
-    description:"Cette plateforme de guidage académique en ligne vous permet de choisir un coach professionnel et de construire ensemble un plan d'études adapté à votre personnalité, vos motivations, vos désirs et vos ambitions !",
-    Link: "https://www.premiercap.fr/",
-  },
-  {
-    id: 3,
+  /*{
+    id: 4,
     title: "De Laure et d'épices",
     description:
       "Partez en voyage avec De Laure et d'Épices, une plateforme de commerce en ligne dédiée à la vente de thés bio de haute qualité, élaborés avec un engagement envers l'agriculture responsable.",
     Link: "https://www.delaureetdepices.fr/",
-  },
+  },*/
     /*{
-      id: 4,
+      id: 5,
       title: "DC-Comiques",
       description:
         "Developer friend, a game where you are the hero.Would you manage to go to the end of your mission ?",
