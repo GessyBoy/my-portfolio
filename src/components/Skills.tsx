a56b2e428e2870d92d4993dcd2c2382306b24c6e
GessyBoy <ydeletrez@gmail.com>
2024-12-01 17:31:20 +0100

    edit skills symfony

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -5,11 +5,11 @@ const Skills = (): JSX.Element => {
         <div className="grid-skills">
           <div className="skill-card html" title="Html5">
             <i className="fa-brands fa-html5 html-icon"></i>
-            <p>HTML</p>
+            <p>HTML5</p>
           </div>
           <div className="skill-card css" title="Sass - TailWind">
             <i className="fa-brands fa-css3-alt css-icon"></i>
-            <p>CSS</p>
+            <p>CSS3</p>
           </div>
           <div className="skill-card js" title="Javascript - Typescript">
             <i className="fa-brands fa-js-square js-icon"></i>
@@ -21,16 +21,20 @@ const Skills = (): JSX.Element => {
           </div>
           <div className="skill-card vuejs" title="Vue.js">
             <i className="fa-brands fa-vuejs vuejs-icon"></i>
-            <p>Vue</p>
+            <p>Vue.js</p>
           </div>
-          <div className="skill-card node" title="Expressjs">
+          <div className="skill-card node" title="Node.js - Express.js">
             <i className="fa-brands fa-node-js node-icon"></i>
-            <p>Node</p>
+            <p>Node.js</p>
           </div>
-          <div className="skill-card php" title="PHP - Symphony">
+          <div className="skill-card php" title="PHP">
             <i className="fa-brands fa-php php-icon"></i>
             <p>PHP</p>
           </div>
+          <div className="skill-card symfony" title="Symfony">
+            <i className="fa-brands fa-symfony symfony-icon"></i>
+            <p>Symfony</p>
+            </div>
           <div className="skill-card wordpress" title="Wordpress">
             <i className="fa-brands fa-wordpress wordpress-icon"></i>
             <p>Wordpress</p>
