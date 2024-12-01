0af1e35974f6deb4bcd39e4b78a85f039a69ee08
GessyBoy <ydeletrez@gmail.com>
2024-12-01 12:25:39 +0100

    change project

--- src/components/data.js
+++ src/components/data.js
@@ -6,19 +6,19 @@ export const projectData = [
       "Ceformed est un centre de formation spécialisé dans le Développement Professionnel Continu (DPC) pour les professionnels de santé, visant à actualiser leurs connaissances et améliorer la prise en charge des patients. Créé en 2017, il propose des formations adaptées à tous les statuts d’exercice en santé.",
     Link: "https://www.ceformed.fr/",
   },
-  {
+  /*{
     id: 2,
     title: "Coloorfool",
     description:"Coolorfool est une marque de chaussettes et mi-bas fabriqués en Italie à partir de fil d’Écosse et de laine mérinos. Les produits sont confectionnés dans un atelier familial, garantissant ainsi un savoir-faire artisanal de qualité. La marque propose ses articles via un site e-commerce.",
     Link: "https://coolorfool.eu/index.php?",
-  },
- /* {
+  },*/
+  {
     id: 3,
     title: "J-imo",
     description:
       "Gestion locative. JIMO automatise vos tâches chronophages tout en offrant un soutien humain pour une gestion simplifiée et efficace de vos biens.",
     Link: "https://app.j-imo.com/",
-  },*/
+  },
   /*{
     id: 4,
     title: "De Laure et d'épices",
