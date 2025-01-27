d4fc2a7fdaca17ef4a39ab6fcb8cba30727bae79
GessyBoy <ydeletrez@gmail.com>
2025-01-27 11:53:25 +0100

    edit message

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -4,8 +4,8 @@ const Contact = () => {
         <h2>Contactez-moi</h2>
         <p>Vous avez un projet à réaliser, des questions à poser ou souhaitez discuter d'une collaboration, n'hésitez pas à me contacter. Je serai ravi de vous accompagner dans vos besoins et de vous répondre rapidement. À très bientôt !
         </p>
-        <a href="mailto:contact@toupa.fr" className='cta-btn'>Envoyez un message</a>
-      </div>
+        <a href="mailto:contact@toupa.fr?subject=Demande%20d'informations&body=Bonjour,%20je%20souhaiterais%20avoir%20plus%20d'informations%20sur%20vos%20prestations." className="cta-btn">Envoyez un message</a>
+        </div>
     )
   }
   
